515c202e9c9fe27ddabbbfdd7f33e7d7df1767f2
amitg01 <amitg.ghy@gmail.com>
2023-10-19 16:09:08 +0530

    added auth

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -21,7 +21,6 @@ const getAllNotes = asyncHandler(async (req, res) => {
   const notesWithUser = await Promise.all(
     notes.map(async (note) => {
       const user = await User.findById(note.user).lean().exec();
-      console.log({ user });
       return { ...note, username: user.username };
     })
   );
